66e57fda841e0655df6816937dd5cc1e7d7d9042
I am Zgoorss <zgoorss@gmail.com>
2022-02-24 20:55:27 +0100

    Add model specs

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -3,6 +3,8 @@
 require 'rails_helper'
 
 describe Customer, type: :model do
+  let(:customer) { create(:customer) }
+
   describe 'validations' do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:address) }
@@ -11,5 +13,28 @@ describe Customer, type: :model do
 
   describe 'associations' do
     it { is_expected.to have_many(:subscriptions) }
+    it { is_expected.to have_many(:payments) }
+  end
+
+  describe '#last_payment_token' do
+    subject(:last_payment_token) { customer.last_payment_token }
+
+    context 'with payments' do
+      let(:payment) { build :payment }
+
+      before do
+        subscription = build :subscription, customer: customer
+        subscription.payments << payment
+        subscription.save
+      end
+
+      it 'returns last payment token' do
+        expect(last_payment_token).to eq(payment.token)
+      end
+    end
+
+    context 'with no payments' do
+      it { is_expected.to be_nil }
+    end
   end
 end
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -3,6 +3,8 @@
 require 'rails_helper'
 
 describe Subscription, type: :model do
+  let(:subscription) { create(:subscription) }
+
   describe 'validations' do
     it { is_expected.to validate_presence_of(:expires_at) }
   end
@@ -11,5 +13,17 @@ describe Subscription, type: :model do
     it { is_expected.to belong_to(:customer) }
     it { is_expected.to belong_to(:plan) }
     it { is_expected.to have_many(:payments) }
+  end
+
+  describe '#new_expires_at_date' do
+    subject(:new_expires_at_date) { subscription.new_expires_at_date }
+
+    context 'when expires_at is today' do
+      before { subscription.expires_at = Date.today }
+
+      it 'extends expires_at by 1 month' do
+        expect(new_expires_at_date).to eq(Date.today + 1.month)
+      end
+    end
   end
 end
